6fc0a3fa0b02a3a7f758ba4471629220e4d3b62d
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-11-10 21:51:29 +0900

    refactor: change input view's readMenu() to readOrderMenu()

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -11,7 +11,7 @@ public class InputView {
         return toInt(input);
     }
 
-    public String readMenu(){
+    public String readOrderMenu(){
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         String input = Console.readLine();
 
