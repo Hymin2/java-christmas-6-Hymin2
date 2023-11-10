72907a2099a3f5a442afd9a6b90a38086ab8a26b
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-11-10 21:56:55 +0900

    feat: add output view instance

--- src/main/java/christmas/controller/RestaurantEventController.java
+++ src/main/java/christmas/controller/RestaurantEventController.java
@@ -1,21 +1,39 @@
 package christmas.controller;
 
+import christmas.service.RestaurantEventService;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class RestaurantEventController {
     private final RestaurantEventService restaurantEventService;
+    private final InputView inputView;
+    private final OutputView outputView;
 
-    public RestaurantEventController(RestaurantEventService restaurantEventService){
+    public RestaurantEventController(RestaurantEventService restaurantEventService,
+                                     InputView inputView,
+                                     OutputView outputView){
         this.restaurantEventService = restaurantEventService;
+        this.inputView = inputView;
+        this.outputView = outputView;
     }
 
     public void saveVisitDay(){
-        restaurantEventService.saveVisitDay();
+        int date = inputView.readDate();
+        restaurantEventService.saveVisitDay(date);
     }
 
     public void saveOrderMenu(){
-        restaurantEventService.saveOrderMenu();
+        String orderMenu = inputView.readOrderMenu();
+        restaurantEventService.saveOrderMenu(orderMenu);
     }
 
     public void printEventBenefits(){
-        restaurantEventService.printEventBenefits();
+        outputView.printOrderMenu();
+        outputView.printTotalPrice();
+        outputView.printServiceMenu();
+        outputView.printBenefit();
+        outputView.printTotalBenefit();
+        outputView.printExpectPrice();
+        outputView.printEventBadge();
     }
 }
