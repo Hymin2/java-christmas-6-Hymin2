230f38a5d72015a28bac17fa8a06ffbeb4116723
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-11-10 22:39:24 +0900

    feat: add type field to menu domain

--- src/main/java/christmas/domain/Menu.java
+++ src/main/java/christmas/domain/Menu.java
@@ -2,9 +2,10 @@ package christmas.domain;
 
 public class Menu {
     private String name;
+    private String type;
     private int price;
 
-    public Menu(String name, int price){
+    public Menu(String name, String type, int price){
         this.name = name;
         this.price = price;
     }
