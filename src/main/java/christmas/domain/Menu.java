82d0651fe0e84d461388184c3b11f881f1b67843
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-11-10 22:07:50 +0900

    feat: add menu domain

---
+++ src/main/java/christmas/domain/Menu.java
@@ -0,0 +1,11 @@
+package christmas.domain;
+
+public class Menu {
+    private String name;
+    private int price;
+
+    public Menu(String name, int price){
+        this.name = name;
+        this.price = price;
+    }
+}
