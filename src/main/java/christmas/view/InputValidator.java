a96ed131d0ffcb73b394c16a17285b6e13917693
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-11-12 14:44:46 +0900

    refactor: split input validator's if-conditions to methods

--- src/main/java/christmas/view/InputValidator.java
+++ src/main/java/christmas/view/InputValidator.java
@@ -3,21 +3,34 @@ package christmas.view;
 public class InputValidator {
     private final String numberRegex = "^[0-9]*$";
     private final String menuOrderRegex = "^[ㄱ-ㅎ|가-힣]+-[0-9]{1,2}";
+
     public void validateNumber(String input){
-        if(!input.matches(numberRegex)) {
+        if(isNotNumber(input)) {
             throw new IllegalArgumentException();
         }
     }
 
     public void validateDate(int input){
-        if(input < 1 || input > 31){
+        if(isNotDateRange(input)){
             throw new IllegalArgumentException();
         }
     }
 
     public void validateMenuOrder(String input){
-        if(!input.matches(menuOrderRegex)){
+        if(isNotCorrectMenuOrder(input)){
             throw new IllegalArgumentException();
         }
+    }
+
+    private boolean isNotCorrectMenuOrder(String input){
+        return !input.matches(menuOrderRegex);
+    }
+
+    private boolean isNotDateRange(int input){
+        return input < 1 || input > 31;
+    }
+
+    private boolean isNotNumber(String input){
+        return !input.matches(numberRegex);
     }
 }
