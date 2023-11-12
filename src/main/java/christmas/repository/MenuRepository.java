1045f6b19cf25d5e5a82c6b91c0e64ad236e06ae
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-11-12 21:52:37 +0900

    feat: update menu repository

--- src/main/java/christmas/repository/MenuRepository.java
+++ src/main/java/christmas/repository/MenuRepository.java
@@ -4,19 +4,25 @@ import christmas.domain.Menu;
 import java.util.ArrayList;
 
 public class MenuRepository {
-    private static ArrayList<Menu> menu;
+    private static ArrayList<Menu> menu = new ArrayList<>();
 
     public void save(Menu _menu){
         menu.add(_menu);
     }
 
     public boolean existMenuByName(String name){
-        return menu.contains(name);
+        for(Menu m : menu){
+            if(m.equals(name)) return true;
+        }
+
+        return false;
     }
 
     public Menu findMenuByName(String name){
         if(existMenuByName(name)) {
-            return menu.get(menu.indexOf(name));
+            for(Menu m : menu){
+                if(m.equals(name)) return m;
+            }
         }
 
         throw new IllegalArgumentException();
