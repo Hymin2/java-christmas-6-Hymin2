0faf6c4a552e7e9726482e8c86d8c3c8d8358c61
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-11-12 21:20:10 +0900

    refactor: modify order domain

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,15 +1,11 @@
 package christmas.domain;
 
-import java.util.ArrayList;
-
 public class Order {
-    private int month;
-    private int day;
-    private ArrayList<Menu> menus;
+    private Menu menu;
+    private int menuNumber;
 
-    public Order(int month, int day, ArrayList<Menu> menus) {
-        this.month = month;
-        this.day = day;
-        this.menus = menus;
+    public Order(Menu menu, int menuNumber) {
+        this.menu = menu;
+        this.menuNumber = menuNumber;
     }
 }
