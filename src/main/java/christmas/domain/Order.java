50a4706a9f0c53bee407f17bf2e6fe3d3a11e2d4
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-11-12 22:46:14 +0900

    feat: modify order and add validate method

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,11 +1,19 @@
 package christmas.domain;
 
-public class Order {
-    private Menu menu;
-    private int menuNumber;
+import java.util.HashMap;
+import java.util.Map;
 
-    public Order(Menu menu, int menuNumber) {
-        this.menu = menu;
-        this.menuNumber = menuNumber;
+public class Order {
+    private Map<Menu, Integer> menu = new HashMap<>();
+
+    public void save(Menu menu, Integer menuNumber){
+        validate(menu);
+        this.menu.put(menu, menuNumber);
+    }
+
+    private void validate(Menu menu){
+        if(this.menu.containsKey(menu)){
+            throw new IllegalArgumentException();
+        }
     }
 }
