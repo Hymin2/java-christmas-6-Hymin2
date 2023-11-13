c5ba0ed1c1dacaa1ba1a82d29a02c081d4da0468
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-11-14 04:00:57 +0900

    feat: add week discount event

---
+++ src/main/java/christmas/event/WeekDiscountEvent.java
@@ -0,0 +1,30 @@
+package christmas.event;
+
+import christmas.config.DayOfWeek;
+import christmas.domain.Order;
+import christmas.util.DateCalculator;
+
+public class WeekDiscountEvent implements DiscountEvent{
+    @Override
+    public int getDiscountPrice(Order order) {
+        return 0;
+    }
+
+    @Override
+    public boolean isApply(Order order) {
+        if(order.getAllPrice() < 10000){
+            return false;
+        }
+
+        if(isNotWeek(DateCalculator.calcDayOfWeek(order.getDate()))){
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean isNotWeek(DayOfWeek dayOfWeek){
+        return  dayOfWeek.equals(DayOfWeek.FRIDAY) ||
+                dayOfWeek.equals(DayOfWeek.SATURDAY);
+    }
+}
