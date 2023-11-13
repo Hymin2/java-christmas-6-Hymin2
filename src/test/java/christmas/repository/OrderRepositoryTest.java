91bac233202e27c913b9f8b2793e1b4870c91c5d
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-11-14 03:43:14 +0900

    test: update order repository test

--- src/test/java/christmas/repository/OrderRepositoryTest.java
+++ src/test/java/christmas/repository/OrderRepositoryTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import christmas.domain.Menu;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,9 +18,9 @@ class OrderRepositoryTest {
         Menu menu2 = new Menu("메뉴2", "디저트", 3000);
         Menu menu3 = new Menu("메뉴3", "메인", 20000);
 
-        orderRepository.save(menu1, 2);
-        orderRepository.save(menu2, 2);
-        orderRepository.save(menu3, 2);
+        orderRepository.saveMenu(menu1, 2);
+        orderRepository.saveMenu(menu2, 2);
+        orderRepository.saveMenu(menu3, 2);
     }
 
     @AfterEach
@@ -35,7 +34,7 @@ class OrderRepositoryTest {
         Menu menu = new Menu("메뉴1", "메인", 30000);
 
         assertThrows(IllegalArgumentException.class, () -> {
-            orderRepository.save(menu, 2);
+            orderRepository.saveMenu(menu, 2);
         });
     }
 
@@ -45,7 +44,7 @@ class OrderRepositoryTest {
         Menu menu = new Menu("메뉴메뉴", "메인", 30000);
 
         assertThrows(IllegalArgumentException.class, () -> {
-            orderRepository.save(menu, 19);
+            orderRepository.saveMenu(menu, 19);
         });
     }
 
@@ -58,9 +57,9 @@ class OrderRepositoryTest {
         Menu menu2 = new Menu("메뉴2", "음료", 3000);
         Menu menu3 = new Menu("메뉴3", "음료", 2000);
 
-        orderRepository.save(menu1, 2);
-        orderRepository.save(menu2, 2);
-        orderRepository.save(menu3, 2);
+        orderRepository.saveMenu(menu1, 2);
+        orderRepository.saveMenu(menu2, 2);
+        orderRepository.saveMenu(menu3, 2);
 
         assertThrows(IllegalArgumentException.class, () -> {
             orderRepository.isAllDrink();
@@ -90,6 +89,4 @@ class OrderRepositoryTest {
 
         assertEquals(2, dessertMenuCount);
     }
-
-
 }
