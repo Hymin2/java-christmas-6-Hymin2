5d82fe8f61b54caabbfc7a98bf4110a4fcee566a
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-11-14 03:56:05 +0900

    feat: add Christmas D-day discount event

---
+++ src/main/java/christmas/event/ChristmasDayDiscountEvent.java
@@ -0,0 +1,28 @@
+package christmas.event;
+
+import christmas.domain.Order;
+
+public class ChristmasDayDiscountEvent implements DiscountEvent{
+
+    @Override
+    public int getDiscountPrice(Order order) {
+        if(isApply(order)){
+            return 1000 + 100 * (order.getDate() - 1);
+        }
+
+        return 0;
+    }
+
+    @Override
+    public boolean isApply(Order order) {
+        if (order.getAllPrice() < 10000){
+            return false;
+        }
+
+        if(order.getDate() > 25){
+            return false;
+        }
+
+        return true;
+    }
+}
