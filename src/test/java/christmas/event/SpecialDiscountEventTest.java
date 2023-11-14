03e490315668ba1cea71384ca7ed390575e7452b
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-11-14 15:41:55 +0900

    test: add special discount event test

---
+++ src/test/java/christmas/event/SpecialDiscountEventTest.java
@@ -0,0 +1,77 @@
+package christmas.event;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import christmas.domain.Menu;
+import christmas.domain.Order;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SpecialDiscountEventTest {
+    private static SpecialDiscountEvent SpecialDiscountEvent;
+
+    @BeforeAll
+    static void setUp(){
+        SpecialDiscountEvent = new SpecialDiscountEvent();
+    }
+
+    @Test
+    @DisplayName("총주문 금액이 10000원 미만일 때 미적용 테스트")
+    void notApplySpecialDiscountEventByLessThanMinPriceTest(){
+        Order order = new Order();
+        order.saveMenu(new Menu("메뉴", "메인", 4000), 1);
+
+        boolean isApply = SpecialDiscountEvent.isApply(order);
+
+        assertEquals(false, isApply);
+    }
+
+    @Test
+    @DisplayName("총주문 금액이 10000원 이상이고 일요일이나 25일이 아닌 경우 미적용 테스트")
+    void notApplySpecialDiscountEventByNotWeekTest(){
+        Order order = new Order();
+        order.saveMenu(new Menu("메뉴", "메인", 10000), 1);
+        order.saveDate(6);
+
+        boolean isApply = SpecialDiscountEvent.isApply(order);
+
+        assertEquals(false, isApply);
+    }
+
+    @Test
+    @DisplayName("총주문 금액이 10000원 이상이고 25일인 경우 적용 테스트")
+    void applyChristmasSpecialDiscountEventTest(){
+        Order order = new Order();
+        order.saveMenu(new Menu("메뉴", "메인", 10000), 1);
+        order.saveDate(25);
+
+        boolean isApply = SpecialDiscountEvent.isApply(order);
+
+        assertEquals(true, isApply);
+    }
+
+    @Test
+    @DisplayName("총주문 금액이 10000원 이상이고 일요일인 경우 적용 테스트")
+    void applySundaySpecialDiscountEventTest(){
+        Order order = new Order();
+        order.saveMenu(new Menu("메뉴", "메인", 10000), 1);
+        order.saveDate(24);
+
+        boolean isApply = SpecialDiscountEvent.isApply(order);
+
+        assertEquals(true, isApply);
+    }
+
+    @Test
+    @DisplayName("특별 할인 테스트")
+    void discountPriceTest(){
+        Order order = new Order();
+        order.saveMenu(new Menu("메뉴", "디저트", 10000), 2);
+        order.saveDate(10);
+
+        int discount = SpecialDiscountEvent.getDiscountPrice(order);
+
+        assertEquals(1000, discount);
+    }
+}
