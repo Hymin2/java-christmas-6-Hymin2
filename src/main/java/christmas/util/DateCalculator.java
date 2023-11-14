8edabd6fce47ef22259bfc76b7a21599f958ed79
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-11-15 02:14:41 +0900

    refactor: add event config enum and modify constant values

--- src/main/java/christmas/util/DateCalculator.java
+++ src/main/java/christmas/util/DateCalculator.java
@@ -1,16 +1,19 @@
 package christmas.util;
 
 import christmas.config.DayOfWeek;
+import christmas.config.EventConfig;
+import christmas.error.ErrorMessage;
 
 public class DateCalculator {
-    private static final int year = 2023;
-    private static final int month = 12;
-    private static final int days = 31;
     private static final DayOfWeek firstDayOfWeek = DayOfWeek.FRIDAY;
 
     public static DayOfWeek calcDayOfWeek(int date){
-        if(date > days){
-            throw new IllegalArgumentException();
+        if(date > EventConfig.DAY_RANGE_MAX_VALUE.getValue()){
+            throw new IllegalArgumentException(ErrorMessage.EXCEED_MAX_ORDER_NUMBER_ERROR_MESSAGE.getMessage());
+        }
+
+        if(date < EventConfig.DAY_RANGE_MIN_VALUE.getValue()){
+            throw new IllegalArgumentException(ErrorMessage.LESS_MIN_ORDER_NUMBER_ERROR_MESSAGE.getMessage());
         }
 
         return DayOfWeek.getNowDayOfWeek(firstDayOfWeek, 1, date);
