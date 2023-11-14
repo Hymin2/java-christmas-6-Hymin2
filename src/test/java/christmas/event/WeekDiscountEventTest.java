65f61ca165ca62dbbaa7a13e45eb7b30176b46dd
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-11-14 15:10:03 +0900

    test: add week discount event test

---
+++ src/test/java/christmas/event/WeekDiscountEventTest.java
@@ -0,0 +1,65 @@
+package christmas.event;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import christmas.domain.Menu;
+import christmas.domain.Order;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WeekDiscountEventTest {
+    private static WeekDiscountEvent weekDiscountEvent;
+
+    @BeforeAll
+    static void setUp(){
+        weekDiscountEvent = new WeekDiscountEvent();
+    }
+
+    @Test
+    @DisplayName("총주문 금액이 10000원 미만일 때 미적용 테스트")
+    void notApplyWeekDiscountEventByLessThanMinPriceTest(){
+        Order order = new Order();
+        order.saveMenu(new Menu("메뉴", "메인", 4000), 1);
+
+        boolean isApply = weekDiscountEvent.isApply(order);
+
+        assertEquals(false, isApply);
+    }
+
+    @Test
+    @DisplayName("총주문 금액이 10000원 이상이고 평일이 아닌 경우 미적용 테스트")
+    void notApplyWeekDiscountEventByNotWeekTest(){
+        Order order = new Order();
+        order.saveMenu(new Menu("메뉴", "메인", 10000), 1);
+        order.saveDate(8);
+
+        boolean isApply = weekDiscountEvent.isApply(order);
+
+        assertEquals(false, isApply);
+    }
+
+    @Test
+    @DisplayName("총주문 금액이 10000원 이상이고 평일인 경우 적용 테스트")
+    void applyWeekDiscountEventTest(){
+        Order order = new Order();
+        order.saveMenu(new Menu("메뉴", "메인", 10000), 1);
+        order.saveDate(4);
+
+        boolean isApply = weekDiscountEvent.isApply(order);
+
+        assertEquals(true, isApply);
+    }
+
+    @Test
+    @DisplayName("디저트 메뉴가 2개일 경우 할인 테스트")
+    void discountPriceTest(){
+        Order order = new Order();
+        order.saveMenu(new Menu("메뉴", "디저트", 10000), 2);
+        order.saveDate(4);
+
+        int discount = weekDiscountEvent.getDiscountPrice(order);
+
+        assertEquals(4046, discount);
+    }
+}
