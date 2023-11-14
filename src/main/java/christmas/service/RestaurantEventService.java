e9abef041feebe9e670d98032a091ef2466ac779
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-11-15 02:16:28 +0900

    feat: add menu clear method

--- src/main/java/christmas/service/RestaurantEventService.java
+++ src/main/java/christmas/service/RestaurantEventService.java
@@ -1,5 +1,6 @@
 package christmas.service;
 
+import christmas.config.MenuType;
 import christmas.domain.Menu;
 import christmas.domain.Order;
 import christmas.dto.OrderBenefitDto;
@@ -37,10 +38,10 @@ public class RestaurantEventService {
         orderRepository.saveDate(date);
     }
 
-    public void saveOrderMenu(String orderMenu) {
+    public void saveOrderMenu(String orderMenu){
         String[] orderMenus = orderMenu.split(",");
 
-        for(String _orderMenu : orderMenus){
+        for (String _orderMenu : orderMenus) {
             String[] nameAndNumber = _orderMenu.split("-");
 
             Menu menu = menuRepository.findMenuByName(nameAndNumber[0]);
@@ -153,24 +154,32 @@ public class RestaurantEventService {
     public int getExpectPrice() {
         Order order = orderRepository.getOrder();
 
-        return order.getAllPrice() - getDiscountPrice();
+        return order.getAllPrice() + getDiscountPrice();
+    }
+
+    public void clearMenu(){
+        menuRepository.clear();
+    }
+
+    public void clearOrder(){
+        orderRepository.clear();
     }
 
     private void initMenu(){
-        this.saveMenu(new Menu("양송이수프", "에피타이저", 6000));
-        this.saveMenu(new Menu("타파스", "에피타이저", 5500));
-        this.saveMenu(new Menu("시저샐러드", "에피타이저", 8000));
+        this.saveMenu(new Menu("양송이수프", MenuType.APPETIZER, 6000));
+        this.saveMenu(new Menu("타파스", MenuType.APPETIZER, 5500));
+        this.saveMenu(new Menu("시저샐러드", MenuType.APPETIZER, 8000));
 
-        this.saveMenu(new Menu("티본스테이크", "메인", 55000));
-        this.saveMenu(new Menu("바비큐립", "메인", 54000));
-        this.saveMenu(new Menu("해산물파스타", "메인", 35000));
-        this.saveMenu(new Menu("크리스마스파스타", "메인", 25000));
+        this.saveMenu(new Menu("티본스테이크", MenuType.MAIN, 55000));
+        this.saveMenu(new Menu("바비큐립", MenuType.MAIN, 54000));
+        this.saveMenu(new Menu("해산물파스타", MenuType.MAIN, 35000));
+        this.saveMenu(new Menu("크리스마스파스타", MenuType.MAIN, 25000));
 
-        this.saveMenu(new Menu("초코케이크", "디저트", 15000));
-        this.saveMenu(new Menu("아이스크림", "디저트", 5000));
+        this.saveMenu(new Menu("초코케이크", MenuType.DESSERT, 15000));
+        this.saveMenu(new Menu("아이스크림", MenuType.DESSERT, 5000));
 
-        this.saveMenu(new Menu("제로콜라", "음료", 3000));
-        this.saveMenu(new Menu("레드와인", "음료", 60000));
-        this.saveMenu(new Menu("샴페인", "음료", 25000));
+        this.saveMenu(new Menu("제로콜라", MenuType.DRINK, 3000));
+        this.saveMenu(new Menu("레드와인", MenuType.DRINK, 60000));
+        this.saveMenu(new Menu("샴페인", MenuType.DRINK, 25000));
     }
 }
