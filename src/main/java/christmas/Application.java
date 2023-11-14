32f3b98699de84087403de690870c9a5bc566ab1
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-11-15 00:16:21 +0900

    feat: add controller instance and run

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,20 @@
 package christmas;
 
+import christmas.controller.RestaurantEventController;
+import christmas.repository.MenuRepository;
+import christmas.repository.OrderRepository;
+import christmas.service.RestaurantEventService;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RestaurantEventController restaurantEventController = new RestaurantEventController(
+                new RestaurantEventService(new MenuRepository(), new OrderRepository()),
+                new InputView(),
+                new OutputView()
+        );
+
+        restaurantEventController.run();
     }
 }
