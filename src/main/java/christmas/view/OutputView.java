78fb4edb48e90015a42e2f4ac689251fb0aa7e1e
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-11-15 00:14:20 +0900

    feat: update output view

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,35 +1,74 @@
 package christmas.view;
 
+import christmas.domain.Menu;
+import christmas.dto.OrderBenefitDto;
+import christmas.dto.OrderMenuDto;
+import java.text.DecimalFormat;
+import java.util.Map;
+
 public class OutputView {
-    public void printEventMessage(){
-        System.out.println("12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+    public void printEventMessage(int date){
+        System.out.println(String.format("%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!", 12, date));
     }
 
-    public void printOrderMenu(){
+    public void printOrderMenu(OrderMenuDto orderMenuDto){
+        System.out.println();
         System.out.println("<주문 메뉴>");
+
+        Map<Menu, Integer> menu = orderMenuDto.menu();
+        for(Menu m : menu.keySet()){
+            System.out.println(m + " " + menu.get(m) + "개");
+        }
     }
 
-    public void printServiceMenu(){
+    public void printServiceMenu(String gift){
+        System.out.println();
         System.out.println("<증정 메뉴>");
+        System.out.println(gift);
     }
 
-    public void printTotalPrice(){
+    public void printTotalPrice(int allPrice){
+        System.out.println();
         System.out.println("<할인 전 총주문 금액>");
+        System.out.println(toWon(allPrice));
     }
 
-    public void printBenefit(){
+    public void printBenefit(OrderBenefitDto orderBenefitDto){
+        System.out.println();
         System.out.println("<혜택 내역>");
+
+        Map<String, Integer> benefits = orderBenefitDto.benefits();
+
+        if(benefits.isEmpty()){
+            System.out.println("없음");
+        }
+
+        for(String benefit : benefits.keySet()){
+            System.out.println(benefit + ": " + toWon(benefits.get(benefit)));
+        }
     }
 
-    public void printTotalBenefit(){
+    public void printTotalBenefit(int benefitPrice){
+        System.out.println();
         System.out.println("<총혜택 금액>");
+        System.out.println(toWon(benefitPrice));
     }
 
-    public void printExpectPrice(){
-        System.out.println("<할인 후 예상 금액>");
+    public void printExpectPrice(int expectPrice){
+        System.out.println();
+        System.out.println("<할인 후 예상 결제 금액>");
+        System.out.println(toWon(expectPrice));
     }
 
-    public void printEventBadge(){
+    public void printEventBadge(String badge){
+        System.out.println();
         System.out.println("<12월 이벤트 배지>");
+        System.out.println(badge);
+    }
+
+    private String toWon(int price){
+        DecimalFormat decimalFormat = new DecimalFormat("###,###");
+
+        return decimalFormat.format(price) + "원";
     }
 }
