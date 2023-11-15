79501310e5fa44c99be8acb8be9315e404bcc661
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-11-15 22:26:51 +0900

    test: modify Christmas badge event test

--- src/test/java/christmas/event/ChristmasBadgeEventTest.java
+++ src/test/java/christmas/event/ChristmasBadgeEventTest.java
@@ -2,26 +2,34 @@ package christmas.event;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import christmas.domain.ApplyEvent;
 import christmas.domain.Order;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ChristmasBadgeEventTest {
     private static ChristmasBadgeEvent christmasBadgeEvent;
+    private static ApplyEvent applyEvent;
 
     @BeforeAll
     static void setUp(){
         christmasBadgeEvent = new ChristmasBadgeEvent();
+        applyEvent = new ApplyEvent();
+    }
+
+    @AfterEach
+    void clear(){
+        applyEvent.clear();
     }
 
     @Test
     @DisplayName("총혜택 금액이 5000원 미만일 때 미적용 테스트")
     void notApplyChristmasBadgeEventByLessThanMinBenefitTest(){
-        Order order = new Order();
-        order.saveBenefit("평일 할인", 3000);
+        applyEvent.saveDiscountBenefit("평일 할인", 3000);
 
-        boolean isApply = christmasBadgeEvent.isApply(order);
+        boolean isApply = christmasBadgeEvent.isApply(applyEvent);
 
         assertEquals(false, isApply);
     }
@@ -29,10 +37,9 @@ class ChristmasBadgeEventTest {
     @Test
     @DisplayName("총혜택 금액이 5000원 이상일 때 적용 테스트")
     void applyChristmasBadgeEventTest(){
-        Order order = new Order();
-        order.saveBenefit("평일 할인", 5000);
+        applyEvent.saveDiscountBenefit("평일 할인", 5000);
 
-        boolean isApply = christmasBadgeEvent.isApply(order);
+        boolean isApply = christmasBadgeEvent.isApply(applyEvent);
 
         assertEquals(true, isApply);
     }
@@ -40,10 +47,9 @@ class ChristmasBadgeEventTest {
     @Test
     @DisplayName("총혜택 금액이 20000원 이상일 때 산타 뱃지 테스트")
     void getSantaBadgeTest(){
-        Order order = new Order();
-        order.saveBenefit("평일 할인", 20000);
+        applyEvent.saveDiscountBenefit("평일 할인", 20000);
 
-        String badge = christmasBadgeEvent.getBadge(order);
+        String badge = christmasBadgeEvent.getBadge(applyEvent);
 
         assertEquals("산타", badge);
     }
@@ -51,10 +57,9 @@ class ChristmasBadgeEventTest {
     @Test
     @DisplayName("총혜택 금액이 10000원 이상일 때 트리 뱃지 테스트")
     void getTreeBadgeTest(){
-        Order order = new Order();
-        order.saveBenefit("평일 할인", 10000);
+        applyEvent.saveDiscountBenefit("평일 할인", 10000);
 
-        String badge = christmasBadgeEvent.getBadge(order);
+        String badge = christmasBadgeEvent.getBadge(applyEvent);
 
         assertEquals("트리", badge);
     }
@@ -62,10 +67,9 @@ class ChristmasBadgeEventTest {
     @Test
     @DisplayName("총혜택 금액이 5000원 이상일 때 별 뱃지 테스트")
     void getStarBadgeTest(){
-        Order order = new Order();
-        order.saveBenefit("평일 할인", 5000);
+        applyEvent.saveDiscountBenefit("평일 할인", 5000);
 
-        String badge = christmasBadgeEvent.getBadge(order);
+        String badge = christmasBadgeEvent.getBadge(applyEvent);
 
         assertEquals("별", badge);
     }
