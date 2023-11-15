45b4733080698271c1a7aece11e6563fd03e995a
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-11-15 22:11:35 +0900

    refactor: modify controller's parameter

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,6 +1,7 @@
 package christmas;
 
 import christmas.controller.RestaurantEventController;
+import christmas.repository.ApplyEventRepository;
 import christmas.repository.MenuRepository;
 import christmas.repository.OrderRepository;
 import christmas.service.RestaurantEventService;
@@ -10,7 +11,7 @@ import christmas.view.OutputView;
 public class Application {
     public static void main(String[] args) {
         RestaurantEventController restaurantEventController = new RestaurantEventController(
-                new RestaurantEventService(new MenuRepository(), new OrderRepository()),
+                new RestaurantEventService(new MenuRepository(), new OrderRepository(), new ApplyEventRepository()),
                 new InputView(),
                 new OutputView()
         );
