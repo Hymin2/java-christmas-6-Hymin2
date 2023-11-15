69dd36781dcbf53a7e1da82c4915a44063edff22
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-11-15 21:43:09 +0900

    feat: add apply event domain

--- src/main/java/christmas/domain/ApplyEvent.java
+++ src/main/java/christmas/domain/ApplyEvent.java
@@ -1,13 +1,40 @@
 package christmas.domain;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ApplyEvent {
-    private Map<String, Integer> benefits = new HashMap<>();
-    private Integer benefitAmount;
-    private Integer discountAmount;
-    private String badge;
+    private final Map<String, Integer> discountBenefits = new HashMap<>();
+    private final Map<String, Integer> giftBenefits = new HashMap<>();
 
+    public void saveDiscountBenefit(String eventName, int benefitAmount){
+        discountBenefits.put(eventName, benefitAmount);
+    }
+    public void saveGiftBenefit(String eventName, int benefitAmount){
+        giftBenefits.put(eventName, benefitAmount);
+    }
 
+    public int getDiscountAmount(){
+        return discountBenefits.values()
+                .stream()
+                .reduce(0, (benefit1, benefit2) -> benefit1 + benefit2)
+                .intValue();
+    }
+
+    public int getAllBenefitAmount(){
+        return discountBenefits.values()
+                .stream()
+                .reduce(0, (benefit1, benefit2) -> benefit1 + benefit2)
+                .intValue() + getDiscountAmount();
+    }
+
+    public Map<String, Integer> getAllBenefits(){
+        Map<String, Integer> allBenefits = new HashMap<>();
+
+        allBenefits.putAll(discountBenefits);
+        allBenefits.putAll(giftBenefits);
+
+        return Collections.unmodifiableMap(allBenefits);
+    }
 }
