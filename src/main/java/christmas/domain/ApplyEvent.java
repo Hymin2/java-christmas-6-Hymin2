f774ebaffad7ffac6d4c361cd2c56ea8ef6d1b85
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-11-15 22:26:01 +0900

    fix: fix getTotalBenefitAmount method

--- src/main/java/christmas/domain/ApplyEvent.java
+++ src/main/java/christmas/domain/ApplyEvent.java
@@ -24,7 +24,7 @@ public class ApplyEvent {
     }
 
     public int getTotalBenefitAmount(){
-        return discountBenefits.values()
+        return giftBenefits.values()
                 .stream()
                 .reduce(0, (benefit1, benefit2) -> benefit1 + benefit2)
                 .intValue() + getDiscountAmount();
